cf1c63d37ba3ead404723ff22d8a825f9ec79d0f
NikOneZ1 <nikita.verba18052004@gmail.com>
2022-06-01 19:42:09 +0300

    Updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,11 +6,11 @@ import twitterLogo from '../media/twitterLogo.svg';
 const Footer = () => {
     return(
         <footer className="footer">
-            <img src={twitterLogo} className="footer-logo" alt="twitter logo" style={{left: "50px"}}/>
-            <img src={githubLogo} alt="github logo" className='footer-logo' style={{left: "150px"}}/>
+            <a href="#"><img src={twitterLogo} className="footer-logo" alt="twitter logo" style={{left: "50px"}}/></a>
+            <a href="https://github.com/NikOneZ1/Blockword"><img src={githubLogo} alt="github logo" className='footer-logo' style={{left: "150px"}}/></a>
             <img src={blockwordWhiteLogo} alt="logo" className='blockword-logo-footer'/>
-            <p style={{right: "50px"}} className='footer-text'>Contact us</p>
-            <p style={{right: "246px"}} className='footer-text'>Lightpaper</p>
+            <a href="mailto:nikita.verba18052004@gmail.com"><p style={{right: "50px"}} className='footer-text'>Contact us</p></a>
+            <a href="https://nikone.notion.site/Lightpaper-ea59c0859ded4337bde4203b8445e1a3"><p style={{right: "246px"}} className='footer-text'>Lightpaper</p></a>
         </footer>
     )
 }
